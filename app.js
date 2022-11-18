12bb3d18fe0e9708dca91f68e0a5162eca1521b1
ddoyediran <ddoyediran@gmail.com>
2022-11-19 00:27:01 +0100

    alternative solution

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const listRoutes = require("./routes");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(listRoutes);
+app.use("/list", listRoutes);
 
-app.get("/", (req, res) => {
-  return res.json("Start with list");
-});
+// app.get("/", (req, res) => {
+//   return res.json("Start with list");
+// });
 
 app.listen("3000", () => {
   console.log("App is listening on port 3000");
--- routes/index.js
+++ routes/index.js
@@ -5,49 +5,92 @@ const listRouter = express.Router();
 const lists = [];
 let id = 0;
 
-// GET all the items on the shopping list
-listRouter.get("/list", (req, res) => {
-  return res.status(200).json({ items: lists });
-});
-
-// GET a single item from the list
-listRouter.get("/list/:id", (req, res) => {
-  const list = lists.find((item) => {
-    return item.id === parseInt(req.params.id);
-  });
-  return res.status(200).json({ list });
-});
-
-// POST an item to the shopping list
-listRouter.post("/list", (req, res) => {
-  lists.push({ ...req.body, id: ++id });
-  return res.status(200).json({ message: "Item added to list" });
-});
-
-// PATCH an item on the list
-listRouter.patch("/list/:id", (req, res) => {
-  const list = lists.find((item) => {
-    return item.id === parseInt(req.params.id);
-  });
-  // updated
-  list.name = req.body.name;
-  list.price = req.body.price;
-  list.weight = req.body.weight;
-
-  return res.status(200).json({ message: "list updated!" });
-});
-
-// DELETE an item from the shopping list
-listRouter.delete("/list/:id", (req, res) => {
-  const itemIndex = lists.findIndex((item) => {
-    return (item.id = parseInt(req.params.id)); // get the index of the item
+// list route
+listRouter
+  .route("")
+  .get((req, res) => {
+    return res.status(200).json({ items: lists });
+  })
+  .post((req, res) => {
+    lists.push({ ...req.body, id: ++id });
+    return res.status(200).json({ message: "Item added to list" });
   });
 
-  lists.splice(itemIndex, 1); // delete the item from the list
+// list/:id route
+listRouter
+  .route("/:id")
+  .get((req, res) => {
+    const list = lists.find((item) => {
+      return item.id === parseInt(req.params.id);
+    });
+    return res.status(200).json({ list });
+  })
+  .patch((req, res) => {
+    const list = lists.find((item) => {
+      return item.id === parseInt(req.params.id);
+    });
+    // updated
+    list.name = req.body.name;
+    list.price = req.body.price;
+    list.weight = req.body.weight;
 
-  return res
-    .status(200)
-    .json({ message: "Item deleted from the shopping list" });
-});
+    return res.status(200).json({ message: "list updated!" });
+  })
+  .delete((req, res) => {
+    const itemIndex = lists.findIndex((item) => {
+      return (item.id = parseInt(req.params.id)); // get the index of the item
+    });
+
+    lists.splice(itemIndex, 1); // delete the item from the list
+
+    return res
+      .status(200)
+      .json({ message: "Item deleted from the shopping list" });
+  });
+
+// // GET all the items on the shopping list
+// listRouter.get("/list", (req, res) => {
+//   return res.status(200).json({ items: lists });
+// });
+
+// // GET a single item from the list
+// listRouter.get("/list/:id", (req, res) => {
+//   const list = lists.find((item) => {
+//     return item.id === parseInt(req.params.id);
+//   });
+//   return res.status(200).json({ list });
+// });
+
+// // POST an item to the shopping list
+// listRouter.post("/list", (req, res) => {
+//   lists.push({ ...req.body, id: ++id });
+//   return res.status(200).json({ message: "Item added to list" });
+// });
+
+// // PATCH an item on the list
+// listRouter.patch("/list/:id", (req, res) => {
+//   const list = lists.find((item) => {
+//     return item.id === parseInt(req.params.id);
+//   });
+//   // updated
+//   list.name = req.body.name;
+//   list.price = req.body.price;
+//   list.weight = req.body.weight;
+
+//   return res.status(200).json({ message: "list updated!" });
+// });
+
+// // DELETE an item from the shopping list
+// listRouter.delete("/list/:id", (req, res) => {
+//   const itemIndex = lists.findIndex((item) => {
+//     return (item.id = parseInt(req.params.id)); // get the index of the item
+//   });
+
+//   lists.splice(itemIndex, 1); // delete the item from the list
+
+//   return res
+//     .status(200)
+//     .json({ message: "Item deleted from the shopping list" });
+// });
 
 module.exports = listRouter;
