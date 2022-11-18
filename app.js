e2cdcf3684eeb6904b30d5e389a0b457d8593850
ddoyediran <ddoyediran@gmail.com>
2022-11-19 00:16:45 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+const listRoutes = require("./routes");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(listRoutes);
+
+app.get("/", (req, res) => {
+  return res.json("Start with list");
+});
+
+app.listen("3000", () => {
+  console.log("App is listening on port 3000");
+});
